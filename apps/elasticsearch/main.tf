877d2cc02b737b98d3cca6080289fe2684a229cc
duongnx <duong.nx@cmctelecom.vn>
2026-05-09 21:33:35 +0700

    combine module asset and es_install

--- apps/elasticsearch/main.tf
+++ apps/elasticsearch/main.tf
@@ -10,28 +10,12 @@ locals {
   inventory = yamldecode(file("${path.module}/inventory.yaml"))
 }
 
-module "common_asset_manager" {
-  source = "../../modules/common_asset_manager"
-
-  environment_id         = var.environment_id
-  service_inventory_file = local.inventory
-
-  # Lấy output từ service_manager
-  service_inventory = module.common_asset_manager.service_inventory
-
-  git_asset_dir = "assets"
-  vm_asset_dir  = "/home/vhv_admin/elasticsearch"
-
-  ssh_user        = "vhv_admin"
-  ssh_private_key = "/home/vhv_admin/.ssh/id_ed25519"
-}
-
-/*module "elasticsearch_installation" {
+module "elasticsearch_installation" {
   source = "../../modules/elasticsearch_installation"
 
   environment_id         = var.environment_id
   service_inventory_file = local.inventory
   service_inventory      = module.elasticsearch_installation.service_inventory
   trigger_atlantis = "20260514T191000Z"
-  vm_asset_dir  = "/home/vhv_admin/elasticsearch"
-}*/
+  es_install_dir  = "/home/vhv_admin"
+}
